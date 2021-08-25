a788a9cf51da325f856ca5675f2f70ac4604a12b
donguty <carlos.joaquin.gutierrez@gmail.com>
2021-08-25 12:21:31 +0200

    modifico pubspec.yml y main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,6 +7,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  Future<String> _loadDatos() async {
+    await Future.delayed(Duration(seconds: 5));
+    return await rootBundle.loadString('assets/datos.txt');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,6 +22,24 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Future builder'),
+        ),
+        body: FutureBuilder(
+          future: _loadDatos(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error : ${snapshot.error.toString()}'),
+              );
+            }
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return Center(
+              child: Text(snapshot.data),
+            );
+          },
         ),
       ),
     );
